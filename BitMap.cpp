25f7fd2dd8560bd4449a9d78f9f75fc409fd6ffc
AlexMecrer <2927518585@qq.com>
2018-06-13 12:57:34 +0800

    Add Delect bitMap

--- BitMap.cpp
+++ BitMap.cpp
@@ -7,11 +7,40 @@ DCBitMapInit(
 )
 {
 	ULONG GbSize = TotalSize.QuadPart >> 30;
-	AimMap->Table = (PMBTABLE)ExAllocatePoolWithTag(NonPagedPool,sizeof(PMBTABLE)*GbSize,'GbS');
+	AimMap->Table = (PMBTABLE*)ExAllocatePoolWithTag(NonPagedPool,sizeof(PMBTABLE)*GbSize,'GbS');
+	AimMap->Size = GbSize;
 	if (AimMap->Table == NULL)
 	{
 		return STATUS_UNSUCCESSFUL;
 	}
 	RtlZeroBytes(AimMap->Table,sizeof(PMBTABLE)*GbSize);
+	return STATUS_SUCCESS;
+}
+
+NTSTATUS 
+DCBitMapDelect(
+	PDC_BITMAP AimMap
+)
+{
+
+	for (int i = 0; i < AimMap->Size; i++)
+	{
+		if (AimMap->Table[i]!=NULL)
+		{
+			for (int j = 0; j < AimMap->Table[i]->Size; j++)
+			{
+				if (AimMap->Table[i]->MbTable[j] != NULL)
+				{
+					for (int z = 0; AimMap->Table[i]->MbTable[j]->Size; z++)
+					{
+						if (AimMap->Table[i]->MbTable[j]->KbTable!= NULL)
+						{
+							ExFreePoolWithTag(AimMap->Table[i]->MbTable[j]->KbTable,'Gbs');//Bug
+						}
+					}
+				}
+			}
+		}
+	}
 	return STATUS_SUCCESS;
 }
--- BitMap.h
+++ BitMap.h
@@ -3,18 +3,32 @@
 
 typedef struct KBTable {//Kb表
 	PCHAR KbTable;
+	ULONG Size;
 }KBTABLE,*PKBTABLE;
 
 
 typedef struct MBTable {//Mb表
-	PKBTABLE MbTable;
+	PKBTABLE* MbTable;
+	ULONG Size;
 }MBTABLE,*PMBTABLE;
 
 typedef struct DCBitMap {//这个是GB表
-	PMBTABLE Table;
+	PMBTABLE* Table;
+	ULONG Size;
 }DC_BITMAP,*PDC_BITMAP;
 
 NTSTATUS DCBitMapInit(
 	PDC_BITMAP AimMap,
 	LARGE_INTEGER TotalSize
+);
+
+NTSTATUS
+DCBitMapDelect(
+	PDC_BITMAP AimMap
+);
+
+NTSTATUS
+DCBitMapQuery(
+	PDC_BITMAP AimMap,
+	LARGE_INTEGER offset
 );
